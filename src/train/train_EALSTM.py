068c58386bed326e51994f3c510fea4a6a83891d
Jared W <willa099@umn.edu>
2021-08-27 19:28:28 -0700

    move to remote

--- src/train/train_EALSTM.py
+++ src/train/train_EALSTM.py
@@ -576,8 +576,9 @@ for epoch in range(n_eps):
 
                 #run model predict
                 h_state = None
-                lstm_net.hidden = lstm_net.init_hidden(batch_size=inputs.size()[0])
-                pred, h_state = lstm_net(inputs, h_state)
+                # lstm_net.hidden = lstm_net.init_hidden(batch_size=inputs.size()[0])
+                pred, h_state, _ = lstm_net(inputs[:,:,n_static_feats:], inputs[:,0,:n_static_feats])
+
                 pred = pred.view(pred.size()[0],-1)
                 pred = pred[:, begin_loss_ind:]
 
