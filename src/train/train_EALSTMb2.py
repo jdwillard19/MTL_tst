25dbe9c9d21babdb5e4dc07e46d021db79abf4bd
Jared W <willa099@umn.edu>
2021-08-31 08:48:30 -0500

    move to remote

--- src/train/train_EALSTMb2.py
+++ src/train/train_EALSTMb2.py
@@ -65,8 +65,8 @@ save = True
 grad_clip = 1.0 #how much to clip the gradient 2-norm in training
 dropout = 0.
 num_layers = 1
-n_hidden = 64
-lambda1 = 0.000
+n_hidden = 256
+lambda1 = 1e-4
 
 
 #epoch settings
